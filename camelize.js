bebdbfb04a277d8d7d1d438f7d4927fa71d384e0
Amal <a.rashid2000@outlook.com>
2025-07-15 04:00:19 +0300

    solved question 19 and added another solution for camelize

--- camelize.js
+++ camelize.js
@@ -1,16 +1,38 @@
 // 18. Camelize
 
+/*
+Translate border-left-width to borderLeftWidth
+Write the function camelize(str) that changes dash-separated words 
+like “my-short-string” into camel-cased “myShortString”.
+That is: removes all dashes, each word after dash becomes uppercased.
+
+Examples:
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+*/
+
+// function camelize(str) {
+// 	let arr = str.split('-');
+// 	let newArr = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (i === 0) {
+// 			newArr.push(arr[i]);
+// 			continue;
+// 		}
+// 		newArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1));
+// 	}
+// 	let camelized = newArr.join('');
+// 	return camelized;
+// }
+
+//another solution
+
 function camelize(str) {
-	let arr = str.split('-');
-	let newArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (i === 0) {
-			newArr.push(arr[i]);
-			continue;
-		}
-		newArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1));
-	}
-	let camelized = newArr.join('');
-	return camelized;
+	return (str
+		.split('-')
+		.map((word, idx) => idx == 0 ? word : word[0].toUpperCase() + word.slice(1))
+		.join('')
+	);
 }
 console.log(camelize("-webkit-transition"));
